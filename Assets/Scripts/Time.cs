3d156ecf427636dbcf00fce2949465de5520e60e
maxvzd <153630971+maxvzd@users.noreply.github.com>
2025-03-09 12:33:06 +0000

    Added timeoutput

--- Assets/Scripts/Time.cs
+++ Assets/Scripts/Time.cs
@@ -1,21 +1,25 @@
-using System;
 using UnityEngine;
 
-public class RotateSun : MonoBehaviour
+public class Time : MonoBehaviour
 {
     [SerializeField] private float minutesInDay;
     [SerializeField, Range(0, 24)] private float hourOfDay;
     [SerializeField] private Transform sunTransform;
+    [SerializeField] private string time;
+
+    public string Get => time;
 
     private float _totalRotation;
 
     private void Update()
     {
-        hourOfDay += Time.deltaTime / (60 * minutesInDay / 24);
+        hourOfDay += UnityEngine.Time.deltaTime / (60 * minutesInDay / 24);
 
         if (hourOfDay > 24) hourOfDay = 0 + (hourOfDay - 24);
         
-        sunTransform.eulerAngles = new Vector3(15 * (hourOfDay - 6), 0f, 0f);
+        sunTransform.eulerAngles = new Vector3(15f * (hourOfDay - 6), 0f, 0f);
+        var seconds = hourOfDay % 1 * 60;
+        time = $"{Mathf.Floor(hourOfDay):00}:{seconds:00}";
     }
 
     private void OnValidate()
